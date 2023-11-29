9f5e771f4a659d923d6cdfc0329e6b361e210275
race5712 <149755365+race5712@users.noreply.github.com>
2023-11-29 19:58:41 +0900

    Update calculator_main.py
    layout_equation_solution 제거.
    
    LineEdit 위젯 생성. 앞선 commit에서는 solution으로 했지만 여기서는 equation으로 수정 후 통일.
    
    입력 값 오른쪽으로 정렬.
    
    레이아웃을 main_layout에 추가.
    
    function 추가 수정(빈 상태에서 = 입력시 error가 뜨던 문제 수정)

--- calculator_main.py
+++ calculator_main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 
 class Main(QDialog):
     def __init__(self):
@@ -13,14 +14,11 @@ class Main(QDialog):
         layout_operation = QHBoxLayout()
         layout_clear_equal = QHBoxLayout()
         layout_number = QGridLayout()
-        layout_equation_solution = QFormLayout()
 
-        ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_solution = QLabel("계산: ")
-        self.solution = QLineEdit("")
-
-        ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addRow(label_solution, self.solution)
+        ### 수식 입력을 위한 LineEdit 위젯 생성
+        label_equation = QLabel("calculate: ")
+        self.equation = QLineEdit("")
+        self.equation.setAlignment(Qt.AlignRight)  # 오른쪽 정렬 설정
 
         ### 사칙연상 버튼 생성
         button_plus = QPushButton("+")
@@ -62,10 +60,10 @@ class Main(QDialog):
             number_button_dict[number] = QPushButton(str(number))
             number_button_dict[number].clicked.connect(lambda state, num = number:
                                                        self.number_button_clicked(num))
-            if number >0:
-                x,y = divmod(number-1, 3)
+            if number > 0:
+                x, y = divmod(number - 1, 3)
                 layout_number.addWidget(number_button_dict[number], x, y)
-            elif number==0:
+            elif number == 0:
                 layout_number.addWidget(number_button_dict[number], 3, 1)
 
         ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
@@ -78,7 +76,8 @@ class Main(QDialog):
         layout_number.addWidget(button_double_zero, 3, 0)
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가
-        main_layout.addLayout(layout_equation_solution)
+        main_layout.addWidget(label_equation)
+        main_layout.addWidget(self.equation)
         main_layout.addLayout(layout_operation)
         main_layout.addLayout(layout_clear_equal)
         main_layout.addLayout(layout_number)
@@ -90,28 +89,30 @@ class Main(QDialog):
     ### functions ###
     #################
     def number_button_clicked(self, num):
-        equation = self.solution.text()
+        equation = self.equation.text()
         equation += str(num)
-        self.solution.setText(equation)
+        self.equation.setText(equation)
 
     def button_operation_clicked(self, operation):
-        equation = self.solution.text()
+        equation = self.equation.text()
         equation += operation
-        self.solution.setText(equation)
+        self.equation.setText(equation)
 
     def button_equal_clicked(self):
-        equation = self.solution.text()
-        solution = eval(equation)
-        self.solution.setText(str(solution))
+        equation = self.equation.text()
+        try:
+            solution = eval(equation)
+            self.equation.setText(str(solution))
+        except Exception as e:
+            self.equation.setText("")
 
     def button_clear_clicked(self):
-        self.solution.setText("")
-        self.solution.setText("")
+        self.equation.setText("")
 
     def button_backspace_clicked(self):
-        equation = self.solution.text()
+        equation = self.equation.text()
         equation = equation[:-1]
-        self.solution.setText(equation)
+        self.equation.setText(equation)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
